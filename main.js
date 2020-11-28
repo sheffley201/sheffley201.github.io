c66836075b5a879af833d457041373815be75d67
Spencer Heffley <spheffley@yahoo.com>
2020-11-28 13:19:49 -0500

    Let the animation play out before sending the user to the next page using location.assign

--- main.js
+++ main.js
@@ -9,23 +9,34 @@ let clickedProject;
 //create a function to animate the picture getting larger
 const picEnlarge = function() {
   if (currentSize < 5000) {
-    currentSize += 500;
+    currentSize += 150;
     projects[clickedProject].style.width = currentSize + 'px';
     projects[clickedProject].style.height = currentSize + 'px';
-    projects[clickedProject].style.top = -currentSize/2 + 'px';
-    projects[clickedProject].style.left = -currentSize/2 + 'px';
+    projects[clickedProject].style.top = -((currentSize - 150) / 2) + 'px';
+    projects[clickedProject].style.left = -((currentSize - 150) / 2) + 'px';
     requestAnimationFrame(picEnlarge);
   } else {
+    //reset size to normal if user opens link in new tab
     currentSize = 100;
     projects[clickedProject].style.width = currentSize + 'px';
     projects[clickedProject].style.height = currentSize + 'px';
     projects[clickedProject].style.top = 0;
     projects[clickedProject].style.left = 0;
   }
+  //send user to desired link after animation plays
+  if (currentSize > 1500) {
+    if (clickedProject == 0) {
+      location.assign('dag-project.html');
+    } else if (clickedProject == 1) {
+      location.assign('ttt-project.html');
+    } else if (clickedProject == 2) {
+      location.assign('sh-project.html');
+    }
+  }
 }
 //event listener determines which project has been clicked
 projectListener.addEventListener('click', whichPic = () => {
-  //event.preventDefault();
+  event.preventDefault();
   clickedProject = Number(event.target.id);
   picEnlarge();
 });
