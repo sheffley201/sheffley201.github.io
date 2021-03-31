60ef412b459e034ba81211c1790fc34f1006c369
Spencer Heffley <spheffley@yahoo.com>
2021-03-31 17:30:30 -0400

    Added pictures for snake and added links to home page. Updated styles to be more future proof.

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
 //pull projects div from index.html
-const projects = document.querySelectorAll(".projects img");
-const projectListener = document.querySelector(".projects");
+const projects = document.querySelectorAll('.projects img');
+const projectListener = document.querySelector('.projects');
 
 //base thumbnail size is 100px
 currentSize = 100;
 let clickedProject;
 
 //create a function to animate the picture getting larger
-const picEnlarge = function () {
+const picEnlarge = function() {
   if (currentSize < 5000) {
-    currentSize += 150;
-    projects[clickedProject].style.width = currentSize + "px";
-    projects[clickedProject].style.height = currentSize + "px";
-    projects[clickedProject].style.top = -((currentSize - 150) / 2) + "px";
-    projects[clickedProject].style.left = -((currentSize - 150) / 2) + "px";
+    currentSize += 150;             
+    projects[clickedProject].style.width = currentSize + 'px';
+    projects[clickedProject].style.height = currentSize + 'px';
+    projects[clickedProject].style.top = -((currentSize - 150) / 2) + 'px';
+    projects[clickedProject].style.left = -((currentSize - 150) / 2) + 'px';
     requestAnimationFrame(picEnlarge);
   } else {
     //reset size to normal if user opens link in new tab
     currentSize = 100;
-    projects[clickedProject].style.width = currentSize + "px";
-    projects[clickedProject].style.height = currentSize + "px";
+    projects[clickedProject].style.width = currentSize + 'px';
+    projects[clickedProject].style.height = currentSize + 'px';
     projects[clickedProject].style.top = 0;
     projects[clickedProject].style.left = 0;
   }
   //send user to desired link after animation plays
   if (currentSize > 1500) {
     if (clickedProject == 0) {
-      location.assign("dag-project.html");
+      location.assign('dag-project.html');
     } else if (clickedProject == 1) {
-      location.assign("ttt-project.html");
+      location.assign('ttt-project.html');
     } else if (clickedProject == 2) {
-      location.assign("sh-project.html");
+      location.assign('sh-project.html');
+    } else if (clickedProject == 3) {
+      location.assign('snake-project.html');
     }
   }
-};
+}
 //event listener determines which project has been clicked
-projectListener.addEventListener(
-  "click",
-  (whichPic = () => {
-    event.preventDefault();
-    clickedProject = Number(event.target.id);
-    picEnlarge();
-  })
-);
+projectListener.addEventListener('click', whichPic = () => {
+  event.preventDefault();
+  clickedProject = Number(event.target.id);
+  picEnlarge();
+});
